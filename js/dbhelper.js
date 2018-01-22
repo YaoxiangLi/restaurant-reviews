e0a084a52b2abf0c19e2988a417d0dde2f0df17e
Yaoxiang Li <lzyacht@gmail.com>
2018-01-22 03:51:52 -0500

    updated get img

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -151,7 +151,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/restaurant-reviews/img/${restaurant.photograph}`);
   }
 
   /**
