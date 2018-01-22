5818635f16d0a9165432ab8ed9305eb0251005bd
Yaoxiang Li <lzyacht@gmail.com>
2018-01-22 03:48:28 -0500

    updated the databaseurl

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,9 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `https://localhost:${port}/data/restaurants.json`;
+    // const port = 8000 // Change this to your server port
+    // return `https://localhost:${port}/data/restaurants.json`;
+    return `https://lzyacht.github.io/restaurant-reviews/data/restaurants.json`;
   }
 
   /**
